b2185229a3c3fe340a8bb3e84748de70ff1357a1
moyola1 <moyola1@hotmail.com>
2026-09-07 16:05:48 -0400

    removed bucket public access and policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,41 +7,41 @@ resource "aws_s3_bucket" "jmoit-static-website" {
   }
 }
 
-resource "aws_s3_bucket_website_configuration" "jmoit-static-website" {
-  bucket = aws_s3_bucket.jmoit-static-website.id
+# resource "aws_s3_bucket_website_configuration" "jmoit-static-website" {
+#   bucket = aws_s3_bucket.jmoit-static-website.id
 
-  index_document {
-    suffix = "index.html"
-  }
+#   index_document {
+#     suffix = "index.html"
+#   }
 
-  error_document {
-    key = "error.html"
-  }
-}
+#   error_document {
+#     key = "error.html"
+#   }
+# }
 
 resource "aws_s3_bucket_public_access_block" "jmoit-static-website" {
   bucket = aws_s3_bucket.jmoit-static-website.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
-resource "aws_s3_bucket_policy" "jmoit-static-website" {
-  bucket = aws_s3_bucket.jmoit-static-website.id
+# resource "aws_s3_bucket_policy" "jmoit-static-website" {
+#   bucket = aws_s3_bucket.jmoit-static-website.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.jmoit-static-website.arn}/*"
-      }
-    ]
-  })
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect    = "Allow"
+#         Principal = "*"
+#         Action    = "s3:GetObject"
+#         Resource  = "${aws_s3_bucket.jmoit-static-website.arn}/*"
+#       }
+#     ]
+#   })
 
-  depends_on = [aws_s3_bucket_public_access_block.jmoit-static-website]
-}
+#   depends_on = [aws_s3_bucket_public_access_block.jmoit-static-website]
+# }
 
