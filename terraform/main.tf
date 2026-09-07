7688d484ea5ddf3afae199cb681ea227a5bfd0db
moyola1 <moyola1@hotmail.com>
2026-09-07 14:04:06 -0400

    add website config to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,14 @@ resource "aws_s3_bucket_website_configuration" "jmoit-static-website" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "jmoit-static-website" {
+  bucket = aws_s3_bucket.jmoit-static-website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 resource "aws_s3_bucket_policy" "jmoit-static-website" {
   bucket = aws_s3_bucket.jmoit-static-website.id
 
@@ -37,11 +45,3 @@ resource "aws_s3_bucket_policy" "jmoit-static-website" {
   depends_on = [aws_s3_bucket_public_access_block.jmoit-static-website]
 }
 
-resource "aws_s3_bucket_public_access_block" "jmoit-static-website" {
-  bucket = aws_s3_bucket.jmoit-static-website.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
